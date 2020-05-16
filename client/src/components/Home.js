23e1304b4f9e876453eb6247530f0db9adaceb2b
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-05-16 14:23:25 +0530

    added landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "../css/home.css";
+import Image from "../assests/VinnovateIT-shape.png";
 const Home = () => {
-  return <div>Landing page</div>;
+  return (
+    <div>
+      <div className="bg"></div>
+      <div className="fadey"></div>
+      <div class="sign">
+        <span class="fast-flicker">b</span>rea<span class="flicker">t</span>he
+      </div>
+      <div className="center">
+        <h1>Amlost before we knew, we left the ground</h1>
+        <div className="buttons">
+          <button className="btn">Wall of Craziness</button>
+          <button className="btn">Wall of Craziness</button>
+          <button className="btn">Wall of Craziness</button>
+          <button className="btn">Wall of Craziness</button>
+        </div>
+      </div>
+      <div className="bottom">
+        <h4>Made with ♥ by VinnovateIT</h4>
+      </div>
+    </div>
+  );
 };
 export default Home;
