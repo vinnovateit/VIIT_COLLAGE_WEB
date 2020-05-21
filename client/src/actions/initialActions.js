3786d1eb6c2dc252b6f8e9f295b78c768dfc590f
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-05-21 16:35:23 +0530

    razor test 4.0

--- client/src/actions/initialActions.js
+++ client/src/actions/initialActions.js
@@ -42,11 +42,33 @@ export const loadingstop = () => {
 
 //razorpay
 export const pay = (a) => (dispatch) => {
-  axios.post("/razorpay", { amount: a }).then(async (res) => {
-    await dispatch(updatedom());
-    dispatch({
-      type: "PAYED",
-      payload: res.data,
-    });
-  });
+  axios
+    .post("/razorpay", { amount: a })
+    .then(async (x) => {
+      await dispatch(updatedom());
+      const amnt = x.data.amount; //amount returned by backend
+      const curr = x.data.currency; //currency returned by backend
+      const idd = x.data.id; //order_id returned by order API
+      var options = {
+        key: "rzp_test_Bqx6B6JlLxprFZ", //add your razorpay account key
+        amount: amnt,
+        currency: curr,
+        name: "",
+        description: "",
+        order_id: idd,
+        handler: function (response) {
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature);
+        },
+        prefill: {
+          name: "",
+          email: "",
+          contact: "",
+        },
+      };
+      var rzp1 = new Razorpay(options);
+      rzp1.open();
+    })
+    .catch((err) => console.log(err));
 };
