cc8eb7782b61ab9e3f250d3deb745fa6edeb6768
wimpywarlord <wimpywarlord@gmail.com>
2020-06-30 15:40:08 +0530

    added hyperlinks to social media , changed names of wall

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,68 +5,69 @@ import { useDispatch, useSelector } from "react-redux";
 import { loaddom, setid } from "../actions/initialActions";
 import RightBar from "./Rightbar";
 const Home = () => {
-  const dispatch = useDispatch();
-  var id = useSelector((state) => state.domReducer.id);
-  // useEffect(() => {
-  //   async function getdata() {
-  //     await dispatch(loaddom());
-  //   }
-  //   getdata();
-  // }, []);
-  const history = useHistory();
-  return (
-    <div>
-      <div className="bg"></div>
-      <div className="fadey"></div>
-      <div class="sign">
-        W<span class="fast-flicker">A</span>LL-<span class="flicker">E</span>
-      </div>
-      <div className="center">
-        <h1>Make your memories eternal through the wheel of time...</h1>
-        <div className="buttons">
-          <button
-            onClick={async () => {
-              await dispatch(setid("5ef8b60d44130f5f9426e9d9"));
-              await dispatch(loaddom("5ef8b60d44130f5f9426e9d9"));
-            }}
-            className="btn btn-secondary btn-home"
-          >
-            Wall of Memories
-          </button>
-          <button
-            onClick={async () => {
-              await dispatch(setid("5ef8b71044130f5f9426e9da"));
-              await dispatch(loaddom("5ef8b71044130f5f9426e9da"));
-            }}
-            className="btn btn-secondary btn-home"
-          >
-            Wall of Riviera
-          </button>
-          <button
-            onClick={async () => {
-              await dispatch(setid("5ef8b7c944130f5f9426e9db"));
-              await dispatch(loaddom("5ef8b7c944130f5f9426e9db"));
-            }}
-            className="btn btn-secondary btn-home"
-          >
-            Wall of Photography
-          </button>
-          <button
-            onClick={async () => {
-              await dispatch(setid("5efacc41cb0e5aa0b29dac5a"));
-              await dispatch(loaddom("5efacc41cb0e5aa0b29dac5a"));
-            }}
-            className="btn btn-secondary btn-home"
-          >
-            Wall of Lockdown moods
-          </button>
+    const dispatch = useDispatch();
+    var id = useSelector((state) => state.domReducer.id);
+    // useEffect(() => {
+    //   async function getdata() {
+    //     await dispatch(loaddom());
+    //   }
+    //   getdata();
+    // }, []);
+    const history = useHistory();
+    return (
+        <div>
+            <div className="bg"></div>
+            <div className="fadey"></div>
+            <div class="sign">
+                W<span class="fast-flicker">A</span>LL-
+                <span class="flicker">E</span>
+            </div>
+            <div className="center">
+                <h1>Make your memories eternal through the wheel of time...</h1>
+                <div className="buttons">
+                    <button
+                        onClick={async () => {
+                            await dispatch(setid("5ef8b60d44130f5f9426e9d9"));
+                            await dispatch(loaddom("5ef8b60d44130f5f9426e9d9"));
+                        }}
+                        className="btn btn-secondary btn-home"
+                    >
+                        Wall of Hostel Memories
+                    </button>
+                    <button
+                        onClick={async () => {
+                            await dispatch(setid("5ef8b71044130f5f9426e9da"));
+                            await dispatch(loaddom("5ef8b71044130f5f9426e9da"));
+                        }}
+                        className="btn btn-secondary btn-home"
+                    >
+                        Mamu Wall
+                    </button>
+                    <button
+                        onClick={async () => {
+                            await dispatch(setid("5ef8b7c944130f5f9426e9db"));
+                            await dispatch(loaddom("5ef8b7c944130f5f9426e9db"));
+                        }}
+                        className="btn btn-secondary btn-home"
+                    >
+                        Wall of Photography
+                    </button>
+                    <button
+                        onClick={async () => {
+                            await dispatch(setid("5efacc41cb0e5aa0b29dac5a"));
+                            await dispatch(loaddom("5efacc41cb0e5aa0b29dac5a"));
+                        }}
+                        className="btn btn-secondary btn-home"
+                    >
+                        Meme Wall
+                    </button>
+                </div>
+            </div>
+            <div className="bottom">
+                <h4>Made with ❤️ by VinnovateIT</h4>
+            </div>
+            <RightBar />
         </div>
-      </div>
-      <div className="bottom">
-        <h4>Made with ❤️ by VinnovateIT</h4>
-      </div>
-      <RightBar />
-    </div>
-  );
+    );
 };
 export default Home;
--- client/src/components/Rightbar.js
+++ client/src/components/Rightbar.js
@@ -1,28 +1,37 @@
 import React from "react";
 import "../css/rightbar.css";
 const RightBar = () => {
-  return (
-    <div className="right-bar">
-      <a href="">
-        <img src="https://image.flaticon.com/icons/svg/174/174857.svg" alt="" />
-      </a>
-      <a href="">
-        <img
-          src="https://image.flaticon.com/icons/svg/2111/2111463.svg"
-          alt=""
-        />
-      </a>
-      <a href="">
-        <img
-          src="https://image.flaticon.com/icons/svg/2111/2111398.svg"
-          alt=""
-        />
-      </a>
-      <a href="">
-        <img src="https://image.flaticon.com/icons/svg/25/25657.svg" alt="" />
-      </a>
-    </div>
-  );
+    return (
+        <div className="right-bar">
+            <a
+                target="_blank"
+                href="https://www.linkedin.com/company/v-innovate-it/"
+            >
+                <img
+                    src="https://image.flaticon.com/icons/svg/174/174857.svg"
+                    alt=""
+                />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/vinnovateit/">
+                <img
+                    src="https://image.flaticon.com/icons/svg/2111/2111463.svg"
+                    alt=""
+                />
+            </a>
+            <a target="_blank" href="https://www.facebook.com/VinnovateIT">
+                <img
+                    src="https://image.flaticon.com/icons/svg/2111/2111398.svg"
+                    alt=""
+                />
+            </a>
+            <a target="_blank" href="https://github.com/vinnovateit">
+                <img
+                    src="https://image.flaticon.com/icons/svg/733/733553.svg"
+                    alt=""
+                />
+            </a>
+        </div>
+    );
 };
 
 export default RightBar;
